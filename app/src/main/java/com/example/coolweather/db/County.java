901a454d1f217b3f7ebe01446082448772a47774
lames <lisiyang44@foxmail.com>
2020-07-07 11:29:09 +0800

    添加数据库表

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public int getCountyCode() {
+        return countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setCountyCode(int countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
