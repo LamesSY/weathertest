ca18d0d7cc70185dbc752ce9ee7041c54168a430
lames <lisiyang44@foxmail.com>
2020-07-09 22:22:23 +0800

    增加显示天气信息功能

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -80,11 +81,6 @@ public class ChooseAreaFragment extends Fragment {
     private City selectCity;
 
     /*
-    * 选中的县
-    * */
-    private County selectCounty;
-
-    /*
     * 当前选中的级别
     * */
     private int currentLevel;
@@ -113,6 +109,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -260,6 +262,8 @@ public class ChooseAreaFragment extends Fragment {
         }
         progressDialog.show();
     }
+
+
 
 
 }
